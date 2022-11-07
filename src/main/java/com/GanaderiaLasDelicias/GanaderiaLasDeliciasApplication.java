ff99f4ee92ed045148e8678b60442198b722f373
José Antonio Solórzano Flores <98479861+Tono84@users.noreply.github.com>
2022-11-06 18:10:43 -0600

    Inicio de Proeyecto final de aplicaciones web

---
+++ src/main/java/com/GanaderiaLasDelicias/GanaderiaLasDeliciasApplication.java
@@ -0,0 +1,13 @@
+package com.GanaderiaLasDelicias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GanaderiaLasDeliciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GanaderiaLasDeliciasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GanaderiaLasDelicias/GanaderiaLasDeliciasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GanaderiaLasDelicias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GanaderiaLasDeliciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
